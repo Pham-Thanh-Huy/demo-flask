e0e816bd66edf82ff59bf8fd286578236e41b5b1
Pham Thanh Huy <huypham3062k3@gmail.com>
2025-03-20 17:09:43 +0700

    [FEAT] Create flask app

---
+++ library/config.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_env = load_dotenv()
+
+MYSQL_HOST = os.getenv("MYSQL_HOST")
+MYSQL_PORT = os.getenv("MYSQL_PORT")
+MYSQL_USERNAME = os.getenv("MYSQL_USERNAME")
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+MYSQL_DATABASE = os.getenv("MYSQL_DATABASE")
+
+SQLALCHEMY_DATABASE_URI = f"mysql://{MYSQL_USERNAME}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}"
+SQLALCHEMY_TRACK_MODIFICATIONS = False
